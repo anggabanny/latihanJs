489e97c0c8628927148d33c02a357e6d39b2cb9f
unknown <anggabanny@gmail.com>
2019-04-24 14:04:09 +0700

    sudah diPerbaiki

--- array_exercise/latihan4.js
+++ array_exercise/latihan4.js
@@ -1,21 +1,44 @@
-function pasanganTerbesar(angka) {
-  var toString = angka.toString();
-  // console.log(angka);
-  var slice='';
+function pasanganTerbesar(number) {
+  var string = number.toString();
+  var length = string.length
+  var max = 0;
 
-  for(i=0;i<1; i++){
-    slice = toString.slice(0,2)
-    console.log(slice);
-    for(j=0;j<1;j++){
-      slice = toString.slice(1,3)
-      console.log(slice);
+  for(i = 0; i < length - 1 ; i++){
+    var slice = string[i] + string [i+1];
+
+    if(max < slice) {
+      max = slice
     }
-    
   }
-  
+  return max
 }
     
 console.log(pasanganTerbesar(12783456)); //83
-// console.log(pasanganTerbesar(9102331)); //91
-// console.log(pasanganTerbesar(71856421)); //85
-// console.log(pasanganTerbesar(79918293)); //99
+console.log(pasanganTerbesar(9102331)); //91
+console.log(pasanganTerbesar(71856421)); //85
+console.log(pasanganTerbesar(79918293)); //99
+
+// -----------------------BEDAH CODE
+// console.log('----------------------------Bedah Code--------------------------------------------');
+
+
+// function bedah(angka) {
+//   var string = angka.toString();
+//   var length = string.length
+//   var max = 0;
+
+//   for(i = 0; i < length - 1 ; i++){
+//     var slice = string[i] + string [i+1];
+//     // console.log(string[i]);
+//     console.log(string[i+1]);
+    
+    
+
+//     if(max < slice) {
+//       max = slice
+//     }
+//   }
+//   return 
+// }
+    
+// console.log(bedah(12783456)); //83
